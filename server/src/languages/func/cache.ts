0c74958830f8fdd2b37e9502f416d2616fd87abc
i582 <51853996+i582@users.noreply.github.com>
2025-07-13 19:29:52 +0400

    extract cache

--- server/src/languages/func/cache.ts
+++ server/src/languages/func/cache.ts
@@ -1,40 +1,10 @@
 //  SPDX-License-Identifier: MIT
 //  Copyright © 2025 TON Core
 import type {NamedNode} from "@server/languages/func/psi/FuncNode"
+import {Cache} from "@server/cache/cache"
 
-export class Cache<TKey, TValue> {
-    private readonly data: Map<TKey, TValue>
-
-    public constructor() {
-        this.data = new Map()
-    }
-
-    public cached(key: TKey, cb: () => TValue): TValue {
-        const cached = this.data.get(key)
-        if (cached !== undefined) {
-            return cached
-        }
-
-        const value = cb()
-        this.data.set(key, value)
-        return value
-    }
-
-    public clear(): void {
-        this.data.clear()
-    }
-
-    public get size(): number {
-        return this.data.size
-    }
-}
-
-export class CacheManager {
-    public readonly resolveCache: Cache<number, NamedNode | null>
-
-    public constructor() {
-        this.resolveCache = new Cache()
-    }
+export class FuncCache {
+    public readonly resolveCache: Cache<number, NamedNode | null> = new Cache()
 
     public clear(): void {
         console.info(`Clearing caches (resolve: ${this.resolveCache.size})`)
@@ -42,4 +12,4 @@ export class CacheManager {
     }
 }
 
-export const FUNC_CACHE = new CacheManager()
+export const FUNC_CACHE = new FuncCache()
--- server/src/languages/tlb/cache.ts
+++ server/src/languages/tlb/cache.ts
@@ -1,40 +1,10 @@
 //  SPDX-License-Identifier: MIT
 //  Copyright © 2025 TON Studio
 import {NamedNode} from "@server/languages/tlb/psi/TlbNode"
+import {Cache} from "@server/cache/cache"
 
-export class Cache<TKey, TValue> {
-    private readonly data: Map<TKey, TValue>
-
-    public constructor() {
-        this.data = new Map()
-    }
-
-    public cached(key: TKey, cb: () => TValue): TValue {
-        const cached = this.data.get(key)
-        if (cached !== undefined) {
-            return cached
-        }
-
-        const value = cb()
-        this.data.set(key, value)
-        return value
-    }
-
-    public clear(): void {
-        this.data.clear()
-    }
-
-    public get size(): number {
-        return this.data.size
-    }
-}
-
-export class CacheManager {
-    public readonly resolveCache: Cache<number, NamedNode[]>
-
-    public constructor() {
-        this.resolveCache = new Cache()
-    }
+export class TlbCache {
+    public readonly resolveCache: Cache<number, NamedNode[]> = new Cache()
 
     public clear(): void {
         console.info(`Clearing caches (resolve: ${this.resolveCache.size})`)
@@ -42,4 +12,4 @@ export class CacheManager {
     }
 }
 
-export const TLB_CACHE = new CacheManager()
+export const TLB_CACHE = new TlbCache()
