f95daffb7497e7b7d431a4a62fc50e1deab85358
Danil Ovchinnikov <danil42worker@gmail.com>
2025-10-20 23:21:34 +0300

    reformat

--- server/src/languages/tolk/tree-sitter-tolk/bindings/node/index.d.ts
+++ server/src/languages/tolk/tree-sitter-tolk/bindings/node/index.d.ts
@@ -1,27 +1,27 @@
 type BaseNode = {
-  type: string;
-  named: boolean;
-};
+    type: string
+    named: boolean
+}
 
 type ChildNode = {
-  multiple: boolean;
-  required: boolean;
-  types: BaseNode[];
-};
+    multiple: boolean
+    required: boolean
+    types: BaseNode[]
+}
 
 type NodeInfo =
-  | (BaseNode & {
-      subtypes: BaseNode[];
-    })
-  | (BaseNode & {
-      fields: { [name: string]: ChildNode };
-      children: ChildNode[];
-    });
+    | (BaseNode & {
+          subtypes: BaseNode[]
+      })
+    | (BaseNode & {
+          fields: {[name: string]: ChildNode}
+          children: ChildNode[]
+      })
 
 type Language = {
-  language: unknown;
-  nodeTypeInfo: NodeInfo[];
-};
+    language: unknown
+    nodeTypeInfo: NodeInfo[]
+}
 
-declare const language: Language;
-export = language;
+declare const language: Language
+export = language
