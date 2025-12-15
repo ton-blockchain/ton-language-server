bcee0549612fd222faddf6aebe6f5b42ee384e30
Danil Ovchinnikov <danil42worker@gmail.com>
2025-12-16 01:47:09 +0300

    feat(tree-sitter-tlb): updated the `tree-sitter` template to `0.25.10` (#215)

--- server/src/languages/tlb/tree-sitter-tlb/grammar.js
+++ server/src/languages/tlb/tree-sitter-tlb/grammar.js
@@ -1,9 +1,8 @@
 /**
- * @file A tree-sitter grammar for the TL-B language
- * @author Petr Makhnev
+ * @file TL-B grammar for tree-sitter
+ * @author TON Core
+ * @author TON Studio
  * @license MIT
- *
- * Based on https://github.com/ton-community/tlb-parser/blob/master/src/grammar/tlb.ts
  */
 
 /// <reference types="tree-sitter-cli/dsl" />
